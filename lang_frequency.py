e9c23004995bcc092656871661dd926c7366bea8
clywe <cheredima@ya.ru>
2016-10-21 21:44:38 +0300

    Добавил поиск 10 самых частых слов

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,30 @@
+import collections
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding="utf8") as myfile:
+        data = myfile.read().replace('\n', '')
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    length = len(text)
+    i = 0
+    word = ""
+    all_words = []
+    while i < length:
+        if str(text[i]).isalpha():
+            word += text[i]
+        else:
+            all_words.append(word)
+            word = ""
+        i += 1
+    all_words.append(word)
+    c = collections.Counter()
+    for word in all_words:
+        c[word] += 1
+    return c.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    print(get_most_frequent_words("load_data("file.txt")"))
